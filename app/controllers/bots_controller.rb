852799d44cb2995a0abe712b192bb917c5325d82
Ian Brennan <ian.brennan@icloud.com>
2015-02-05 14:57:46 -0700

    created bots page with dropdown menu

---
+++ app/controllers/bots_controller.rb
@@ -0,0 +1,4 @@
+class BotsController < ApplicationController
+  def index
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  #alidates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
+  #validates :email, :presence => true, :uniqueness => true
+  #validates :password, :confirmation => true #password_confirmation attr
+  #validates_length_of :password, :in => 6..20, :on => :create
 end
